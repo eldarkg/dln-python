5563e34c9fe90753cda5e688bc51a83b78b2bc00
Eldar Khayrullin <eldar.khayrullin@mail.ru>
2017-07-30 14:52:33 +0300

    test: change

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-import dln
+from dln import *
 
-c = dln.Client()
-c.connect('localhost', dln.DEFAULT_SERVER_PORT)
-d = dln.Device(c, 0)
+c = Client()
+c.connect('localhost', DEFAULT_SERVER_PORT)
+d = Device(c, 0)
 print(d.get_server_version())
 print(c.get_device_count())
-d = c.open_device(0)
+spim = SpiMaster(c, 0)
+print(spim.read_write_16(1, (1, 2, 3)))
+print(spim.read_write_ex(1, b'aabbcc', SpiMaster.ATTR_RELEASE_SS))
+spim.set_frequency(1, 1000000)
+spim.set_frame_size(1, SpiMaster.FRAME_SIZE_16)
+spim.set_mode(1, SpiMaster.MODE_CPHA_1 | SpiMaster.MODE_CPOL_1)
+print(spim.enable(1))
+print(spim.get_port_count())
+h = c.open_device(0)
