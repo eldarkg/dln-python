4a942713c6f0f8006df4313011ab4b874ce41036
Eldar Khayrullin <eldar.khayrullin@mail.ru>
2017-07-31 14:38:24 +0300

    test: refactor

--- test.py
+++ test.py
@@ -4,15 +4,18 @@ from dln import *
 
 c = Client()
 c.connect('localhost', DEFAULT_SERVER_PORT)
-d = Device(c, 0)
-print(d.get_server_version())
 print(c.get_device_count())
-spim = SpiMaster(c, 0)
-print(spim.read_write_16(1, (1, 2, 3)))
-print(spim.read_write_ex(1, b'aabbcc', SpiMaster.ATTR_RELEASE_SS))
-spim.set_frequency(1, 1000000)
-spim.set_frame_size(1, SpiMaster.FRAME_SIZE_16)
-spim.set_mode(1, SpiMaster.MODE_CPHA_1 | SpiMaster.MODE_CPOL_1)
-print(spim.enable(1))
-print(spim.get_port_count())
+
 h = c.open_device(0)
+d = Device(c, h)
+print(d.get_server_version())
+
+spim = SpiMaster(c, h)
+print(spim.get_port_count())
+
+spim.set_frequency(0, 2000000)
+spim.set_frame_size(0, SpiMaster.FRAME_SIZE_16)
+spim.set_mode(0, SpiMaster.MODE_CPHA_1 | SpiMaster.MODE_CPOL_1)
+print(spim.enable(0))
+print(spim.read_write_16(0, (1, 2, 3)))
+print(spim.read_write_ex_16(0, b'aabbcc', SpiMaster.ATTR_RELEASE_SS))
