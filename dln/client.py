87160facfdd76fb0d413a4eeb0001a133309a139
Eldar Khayrullin <eldar.khayrullin@mail.ru>
2024-11-24 16:19:53 +0300

    Fix copyright

--- dln/client.py
+++ dln/client.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  Eldar Khayrullin <eldar.khayrullin@mail.ru>
+# Copyright (C) 2017-2024  Eldar Khayrullin <eldar.khayrullin@mail.ru>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
